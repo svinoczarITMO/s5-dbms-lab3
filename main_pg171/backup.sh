ffa897fb2356ce610b3a10810d20e94dacc5828a
svinoczar <alexsupper75@gmail.com>
2025-02-02 01:31:26 +0300

    feat: rewrite backup.sh
    Finished 2 of 4 steps from lab task

--- main_pg171/backup.sh
+++ main_pg171/backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Устанавливаем текущую дату для резервной копии
-CURRENT_DATE=$(date "+%Y-%m-%d_%H:%M:%S")
-
-# Создаем полную резервную копию с сжатием
-pg_dump -U postgres1 -d coolyellowsoup -p 9468 | gzip > ~/backups/backup_$CURRENT_DATE.sql.gz
-
-BACKUP_FILE=~/backups/backup_$CURRENT_DATE.sql.gz
-
-# Перемещаем резервную копию на резервный узел
-scp "$BACKUP_FILE" postgres1@pg184:~/backups/
-
-# Удаляем резервные копии старше 28 дней на резервном узле
-ssh postgres1@pg184 'find ~/backups/ -type f -mtime +28 -exec rm -f {} \;'
+DB_NAME="coolyellowsoup"
+DB_USER="postgres1"
+DB_HOST="127.0.0.1"
+BACKUP_DIR="/var/db/postgres1/db_backups"
+REMOTE_HOST="postgres1@pg184"
+REMOTE_DIR="/var/db/postgres1/backups"
+RETENTION_DAYS=28
+mkdir -p "$BACKUP_DIR"
+DATE=$(date +%Y-%m-%d)
+DUMP_FILE="${DB_NAME}_backup_${DATE}.sql.gz"
+pg_dump -U "$DB_USER" "$DB_NAME" -p 9468 | gzip > "${BACKUP_DIR}/${DUMP_FILE}"
+rsync -av --remove-source-files "${BACKUP_DIR}/${DUMP_FILE}" "${REMOTE_HOST}:${REMOTE_DIR}"
+ssh "${REMOTE_HOST}" "find ${REMOTE_DIR} -type f -name '*.sql.gz' -mtime +${RETENTION_DAYS} -delete"
